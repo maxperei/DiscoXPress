d203ad78e3991e738d222951e3219b1cb1abd969
maxperei <mpl7@hotmail.fr>
2016-12-13 22:06:22 +0100

    implement basic oauth connection w/ express-session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,19 +15,35 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('trust proxy', 1); // trust first proxy
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
+
+app.use(cookieParser());
+
+app.use(session({
+  resave: false,
+  secret: 'gianniguigou',
+  saveUninitialized: true,
+  cookie: {
+    originalMaxAge: 100000,
+    secure: true
+  }
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true,
   sourceMap: true
 }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/disco.js
+++ routes/disco.js
@@ -2,12 +2,87 @@ var express = require('express');
 var router = express.Router();
 var Discogs = require('disconnect').Client;
 
-/* GET disconnect page. */
+var dis = new Discogs(
+  'Sample/0.1',
+  { consumerKey: 'PKYuNxmfwmpSCYYGFFLB' },
+  { consumerSecret: 'OiiUmZTzdfrQwDdyzDFkEADKNTcKFdTg' }
+);
+
+var sess = {};
+
+/* GET `disco` page. */
 router.get('/', function(req, res, next) {
-  var db = new Discogs().database();
-  db.getRelease(176126, function(err, data){
-    res.send(data);
+  if(!sess.dataAccessed) {
+    res.render('disco', {
+      title: 'Disconnect',
+      author: 'bartve',
+      git: 'https://github.com/bartve/disconnect.git',
+      sessionDebug: JSON.stringify(req.session)
+    });
+  }else{
+    console.log(sess);
+    var col = new Discogs(sess.dataAccessed).user().collection();
+    col.getReleases(sess.username, 0, {page: 1, per_page: 75}, function(err, data){
+      res.render('disco', {
+        title: 'Welcome to '+sess.username+'\'s Library',
+        author: 'maxperei',
+        releases: JSON.stringify(data.releases)
+      });
+    });
+  }
+});
+
+router.get('/authorize', function(req, res, next) {
+  var oAuth = new Discogs().oauth();
+  oAuth.getRequestToken(
+    'PKYuNxmfwmpSCYYGFFLB',
+    'OiiUmZTzdfrQwDdyzDFkEADKNTcKFdTg',
+    'http://local.dev:3000/disco/callback',
+    function(err, requestData){
+      // Persist "requestData" here so that the callback handler can
+      // access it later after returning from the authorize url
+      sess = req.session;
+      sess.dataRequested = requestData;
+      res.redirect(requestData.authorizeUrl);
+    }
+  );
+});
+
+router.get('/callback', function(req, res, next){
+  var oAuth = new Discogs(sess.dataRequested).oauth();
+  oAuth.getAccessToken(
+    req.query.oauth_verifier, // Verification code sent back by Discogs
+    function(err, accessData){
+      // Persist "accessData" here for following OAuth calls
+      sess = req.session;
+      sess.dataAccessed = accessData;
+      res.redirect('/disco/identity');
+    }
+  );
+});
+
+router.get('/identity', function(req, res, next){
+  var dis = new Discogs(sess.dataAccessed);
+  dis.getIdentity(function(err, data){
+    sess.identity = data;
+    sess.username = data.username;
+    res.render('disco', {
+      title: sess.username+'\'s Identity',
+      author: 'maxperei',
+      identity: JSON.stringify(sess.identity)
+    });
   });
 });
+
+router.get('/image', function(req, res, next) {
+  var db = new Discogs(sess.dataAccessed).database();
+  db.getRelease(176126, function(err, data){
+    var url = data.images[0].resource_url;
+    db.getImage(url, function(err, data, rateLimit){
+      // Data contains the raw binary image data
+      res.send(data);
+    });
+  });
+})
 
 module.exports = router;
