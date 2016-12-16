bc8c5853805a8894f375c25bdf99ac4a2d5b1453
maxperei <mpl7@hotmail.fr>
2016-12-16 23:46:53 +0100

    server trypescript translation

---
+++ server/config.ts
@@ -0,0 +1,14 @@
+export const serverPort = 3000;
+export const secret = 'RbBQqA6uF#msRF8s7h*?@=95HUm&DgMDd6zLFn4XzWQ6dtwXSJwBX#?gL2JWf!';
+export const length = 128;
+export const digest = 'sha256';
+export const token = 'HPGAlpxCDTiOminOiLgQejfpihmtGmRziCynzPqS';
+export const key = {
+    'consumerKey': 'tKJDUtRDoJpIDNsIQHCm',
+    'consumerSecret': 'CiHUbhnJpOqdMUeERvsFZBYNpKawZwlW'
+};
+export const headers = {
+    'Accept': 'application/vnd.discogs.v2.plaintext+json',
+    'userAgent': 'Sample/0.1'
+};
+//export const username : 'Dr_Manhattan';
