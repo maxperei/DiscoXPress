cce19c3f6756c5f61403d6ec8f3f37abfdd1c4ba
maxperei <mpl7@hotmail.fr>
2017-01-18 17:04:52 +0100

    new stable w/ native binding

--- client/pages/collection/collection.component.ts
+++ client/pages/collection/collection.component.ts
@@ -20,12 +20,13 @@ export class Collection implements OnInit {
   constructor(public discogs: DiscogsApi, private router: Router){
     this.model.pP = this.choice[1];
     discogs.loadDisco(1, this.model.pP);
-    discogs.libraryObs.subscribe(
+    this.library = discogs;
+    /*discogs.libraryObs.subscribe(
       (data) => {
         this.library = data;
         this.pages = this.library.releases.pagination.pages;
       }
-    );
+    );*/
   }
 
   ngOnInit() { }
